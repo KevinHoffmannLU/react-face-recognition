a563590320329266739aa0fca3dd53d62de77c0c
Kevin <contact@hoffmannkevin.com>
2024-02-11 17:02:20 +0100

    adding icon to logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
 import './Logo.css';
+import brain from './brain.png';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0" style={{width: '150px'}}>
-            <Tilt className="br2 shadow-2 Tilt"  tiltMaxAngleX = {35} tiltMaxAngleY = {35}>
-                <div style={{ height: '150px', width: '150px'}}>
-                    <h1>👀</h1>
+            <Tilt className="br2 shadow-2 Tilt"  tiltMaxAngleX = {30} tiltMaxAngleY = {30}>
+                <div className="pa3" style={{ height: '150px', width: '150px'}}>
+                    <img  src={brain} alt="logo" />
                 </div>
             </Tilt>
         </div>
