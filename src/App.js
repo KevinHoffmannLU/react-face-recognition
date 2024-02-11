295d3fe2af1e834392399511401163d7bf9641df
Kevin <contact@hoffmannkevin.com>
2024-02-11 18:18:57 +0100

    adding background animations using particles-bg package

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div className="App">
+      <>
+        <div>
+        <ParticlesBg type="cobweb" num={20} bg={true} className="particles"/>
+        </div>
+      </>
      <Navigation />
      <Logo />
+     <Rank />
      <ImageLinkForm />
      {/* {<FaceRecognition />} */}
     </div>
