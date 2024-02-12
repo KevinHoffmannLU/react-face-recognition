4a12429c96c125c98e03149b420611031bb4a4ea
Kevin <contact@hoffmannkevin.com>
2024-02-12 16:38:05 +0100

    adding clarifai api

--- src/App.js
+++ src/App.js
@@ -4,8 +4,57 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import { useState } from 'react';
 
 function App() {
+
+  const [imageURL, setIMAGE_URL] = useState('');
+  const [input, setInput] = useState('');
+
+  const returnClarifaiRequestOptions = (imageURL) => {
+    const PAT = '361358e1cff94b8fbd688eb16ec82151';
+    const USER_ID = 'kevinhoffmannlu';       
+    const APP_ID = 'SmartBrain';  
+    const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+    
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+    return requestOptions;
+  }
+  function handleChange(event){
+    setInput(event.target.value);
+  }
+
+  function onButtonSubmit() {
+    setIMAGE_URL(input);
+    fetch("https://api.clarifai.com/v2/models/face-detection/outputs", returnClarifaiRequestOptions(imageURL))
+        .then(response => response.json())
+        .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+        .catch(error => console.log('error', error));
+  }
   return (
     <div className="App">
       <>
@@ -16,8 +65,11 @@ function App() {
      <Navigation />
      <Logo />
      <Rank />
-     <ImageLinkForm />
-     {/* {<FaceRecognition />} */}
+     <ImageLinkForm 
+     handleChange={handleChange} 
+     onButtonSubmit={onButtonSubmit} 
+     />
+     <FaceRecognition imageURL={imageURL}/>
     </div>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Navigation.css";
 
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f3 link dim pa3 pointer br2 shadow-2 ma2">Sign Out</p>
+            <p className="nav-button f3 link dim pa3 pointer br2 shadow-2 ma2">Sign Out</p>
         </nav>
     );
 }
